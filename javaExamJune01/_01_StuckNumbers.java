4ec55ee6655ccb11adb1c3c46be0195b5c957993
sishub <desikaradzhova@gmail.com>
2014-09-14 21:08:05 +0300

    Return the original name class

--- javaExamJune01/_01_StuckNumbers.java
+++ javaExamJune01/_01_StuckNumbers.java
@@ -2,7 +2,7 @@ package javaExamJune01;
 
 import java.util.Scanner;
 
-public class _01_StuckNumbersSecondChange {
+public class _01_StuckNumbers {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
